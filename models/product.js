837307e1fcae2f42c4517436e30f8fb6f632f71d
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2024-12-01 22:35:39 +0530

    update product details

--- models/product.js
+++ models/product.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose"
 
 const productModel = mongoose.Schema({
     productname: String,
+    Description:String,
+    MFD_Date:Date,
+    EXP_Date:Date,
     price:Number,
-    quantity:Number
+    quantity:Number,
 
 })
 const product = mongoose.model("product",productModel)
