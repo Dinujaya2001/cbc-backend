3f22c4983d4f7b64e36129d9a218be35cfd598c1
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2024-12-30 22:41:34 +0530

    login validation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import jwt, { decode } from "jsonwebtoken";
 import dotenv from "dotenv";
