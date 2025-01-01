584282ed9a88b453724bb6b1e9a9ce429716a7ef
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2025-01-01 08:57:06 +0530

    create product model

---
+++ models/product.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+ const productSchema = mongoose.Schema({
+    productID :{
+        type : String,
+        required:true,
+        unique:true
+    },
+    producName :{
+        type : String,
+        required:true,
+        
+    },
+    altName :[
+        {
+        type : String
+        }
+    ], 
+    images :[
+        {
+            type:String
+        }
+    ] ,
+    price :{
+        type : Number,
+        required:true,
+        
+    },
+    lastPrice :{
+        type : Number,
+        required:true,
+        
+    },
+    stock :{
+        type : Number,
+        required:true,
+        
+    },
+    Description :{
+        type : String,
+        required:true,
+        
+    },
+
+ })
+ const product = mongoose.model("products",productSchema);
+ export default product;
