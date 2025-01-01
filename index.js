fee44b20bd731e016ce003d23344d1ac6a5a3f75
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2025-01-01 09:38:40 +0530

    product router connect inndex.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
 import jwt, { decode } from "jsonwebtoken";
 import dotenv from "dotenv";
+import productRouter from './routers/productRouter.js';
 dotenv.config()
 
 const app = express();
@@ -45,6 +46,7 @@ app.use(
 )
 
 app.use("/api/users", userRouter)
+app.use("/api/products",productRouter)
 
 app.listen(
     5000,
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,7 +1,9 @@
-import express from `express`;
+import express from 'express';
 import { createProduct, getProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProduct)
+
+export default productRouter;
