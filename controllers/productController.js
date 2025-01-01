e008fd6456555ef8f12af440a08b9b1ba324ec55
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2025-01-01 09:21:49 +0530

    create product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,24 @@
 import product from "../models/product.js";
 export function createProduct(req,res){
+   /* if (!isAdmin(req)) {
+        res.jeson({
+            message:"Please login as administrator  to add products"
+        })
+        return
+    }
+    const newProductData = res.body
+
+    const product = new product(newProductData)
+
+    product.save().then(()=>{
+        res.jeson({
+            message:"Product is created"
+        })
+    }).catch((error)=>{
+        res.jeson({
+            message:"Product not created"
+        })
+    })
+        
     
 }
