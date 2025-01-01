93862976c6673fd9837b40e3618e7cf8ff9795cf
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2025-01-01 09:33:03 +0530

    product controller | createproduct and get product

---
+++ routers/productRouter.js
@@ -0,0 +1,7 @@
+import express from `express`;
+import { createProduct, getProduct } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.post("/",createProduct)
+productRouter.get("/",getProduct)
